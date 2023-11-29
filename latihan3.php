084189721fa5818e2c1386e108988da4c674d319
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2023-11-29 09:56:02 +0700

    #Task_10

--- latihan3.php
+++ latihan3.php
@@ -1,12 +1,24 @@
 <?php 
 
-    $x = 20;
-    if ($x < 20) {
-        echo "benar";
+    $nilai = 95;
+    if ($nilai > 0 && $nilai < 50) {
+        echo "Nilai : E";
     } 
-    else if($x == 20){
-        echo "Bingo!";
+    else if($nilai >= 50 && $nilai < 60){
+        echo "Nilai : D";
+    }
+    else if($nilai >= 60 && $nilai < 70){
+        echo "Nilai : C";
+    }
+    else if($nilai >= 70 && $nilai < 80){
+        echo "Nilai : B";
+    }
+    else if($nilai >= 80 && $nilai < 90){
+        echo "Nilai : A";
+    }
+    else if($nilai >= 90){
+        echo "Nilai : A+";
     }
     else {
-        echo "salah";
+        echo "inputan nilai salah!";
     }
