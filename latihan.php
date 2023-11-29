fb01111bb6bbada3c6540c0966eaa7e3b884de37
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2023-11-29 09:39:32 +0700

    #Task_3

--- latihan.php
+++ latihan.php
@@ -1,7 +1,13 @@
 <?php 
 
-    for ($i=0; $i < 5; $i++) { 
-        echo "Hai, saya urutan ke - ". $i ."<br>";
+    // for ($i=0; $i < 5; $i++) { 
+    //     echo "Hai, saya urutan ke - ". $i ."<br>";
+    // }
+
+    $a = 0;
+    while ($a < 5) {
+        echo "Halo dunia ! <br>";
+        $a++;
     }
 
 ?>
